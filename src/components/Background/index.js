7e5ee9c1bf6141cfaad5d3fffabb5665e30607f9
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-03-29 17:26:10 -0300

    ⚡  improve: update to receive newLocation prop

--- src/components/Background/index.js
+++ src/components/Background/index.js
@@ -1,10 +1,10 @@
 import linearGradient from 'react-native-linear-gradient';
 import styled from 'styled-components/native';
 
-export default styled(linearGradient).attrs({
+export default styled(linearGradient).attrs(props => ({
   colors: ['#7D40E7', '#fff'],
-  locations: [0.3, 0.3],
-})`
+  locations: props.newLocation || [0.3, 0.3],
+}))`
   flex: 1;
   padding: 20px 20px 0;
 `;
